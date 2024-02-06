ca6f3e896bd41e8b809600f556758b99f6314a6d
RTDevOps <RTDevOps@dmspro.vn>
2024-02-06 17:44:55 +0700

    major updates

--- server/Routes/AdminRoute.js
+++ server/Routes/AdminRoute.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import con from '../utils/db.js';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import multer from 'multer';
+import path from 'path';
 
 const router = express.Router();
 
 router.post('/adminlogin', (req, res)=>{
-  const sql = "SELECT * FROM employeems.admin WHERE email=? AND password=?";
+  const sql = "SELECT * FROM admin WHERE email=? AND password=?";
   con.query(sql, [req.body.email, req.body.password], (err, result)=>{
     if(err) return res.json({loginStatus: false, Error:"Query error"})
     if(result.length > 0) {
@@ -20,6 +23,55 @@ router.post('/adminlogin', (req, res)=>{
     } else {
       return res.json({loginStatus: false, Error: "Wrong email or password"});
     }
+  });
+});
+
+router.get('/category', (req, res)=>{
+  const sql = "SELECT * FROM category";
+  con.query(sql, (err, result)=>{
+    if(err) return res.json({Status:false, Error: "Query Error"});
+    return res.json({Status: true, Result: result});
+  });
+});
+
+router.post('/add_category', (req, res)=>{
+  const sql = "INSERT INTO category (`name`) VALUES (?)";
+  con.query(sql, [req.body.category], (err, result)=>{
+    if(err) return res.json({Status: false, Error: "Query Error"});
+    return res.json({Status: true});
+  });
+});
+
+// image upload
+const storage = multer.diskStorage({
+  destination: (req, file, cb) =>{
+    cb(null, 'Public/Images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname));
+  }
+});
+const upload = multer({storage: storage});
+
+router.post('/add_employee', upload.single('image'), (req, res)=>{
+  console.log(req.body)
+  const sql = "INSERT INTO employees (name, email, password, address, salary, image, category_id) VALUES (?)";
+  bcrypt.hash(req.body.password, 10, (err, hash)=>{
+    if(err) return res.json({Status: false, Error: "Query Error"});
+    const values = [
+      req.body.name,
+      req.body.email,
+      hash,
+      req.body.address,
+      req.body.salary,
+      req.file.filename,
+      req.body.category_id
+    ];
+    console.log(req.body)
+    con.query(sql, [values], (err, result)=>{
+      if(err) return res.json({Status: false, Error: "Query Error"});
+      return res.json({Status: true});
+    });
   });
 });
 
